beb7396c57c1a064c416be7598546f45da5b43a8
Nam <email-cua-m@example.com>
2026-06-08 11:26:39 +0700

    tailwind and primeng

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# TodoAp
+# TodoApp
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 21.2.14.
 
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,18 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { ApplicationConfig } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: 'none'
+        }
+      }
+    })
   ]
 };
