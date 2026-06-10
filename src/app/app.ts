de96022fda5482cf106ff30496ac6f00dcd7011c
Nam <email-cua-m@example.com>
2026-06-10 12:19:50 +0700

    reafactor structure

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Todoapp');
+  protected readonly title = signal('TodoApp');
 }
