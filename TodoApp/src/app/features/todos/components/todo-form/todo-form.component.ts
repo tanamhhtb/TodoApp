0b2234e77420f21158d3738880aed850190ae53b
Nam <email-cua-m@example.com>
2026-06-10 11:21:28 +0700

    feat: setup structure

---
+++ TodoApp/src/app/features/todos/components/todo-form/todo-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-form',
+  imports: [],
+  templateUrl: './todo-form.component.html',
+  styleUrl: './todo-form.component.scss',
+})
+export class TodoFormComponent {}
